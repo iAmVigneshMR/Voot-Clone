72e05a4568b97b446b2e21fc7748b7229817401d
its-GoH <74853413+its-GoH@users.noreply.github.com>
2021-07-12 11:48:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -11,12 +11,12 @@ import "firebase/storage";
 
 // from website (goto setting > project setting > scroll end > select config copy and paste below)
 const firebaseConfig = {
-    apiKey: "AIzaSyDltpYmBYXlaFF_toAgDIKa-fZ7B1GHmrc",
-    authDomain: "voot-c4a6c.firebaseapp.com",
-    projectId: "voot-c4a6c",
-    storageBucket: "voot-c4a6c.appspot.com",
-    messagingSenderId: "841040863982",
-    appId: "1:841040863982:web:6615fbca2fc2dea3e349ff"
+    apiKey: "AIzaSyDltpYmaFF_toDIKa-fZ1GHmrc",
+    authDomain: "voot-c6c.firebeapp.com",
+    projectId: "voot-c6c",
+    storageBucket: "voot-c6c.appspot.com",
+    messagingSenderId: "841063982",
+    appId: "1:84103982:web:6615fbca2fce349ff"
   };
 
   firebase.initializeApp(firebaseConfig);
